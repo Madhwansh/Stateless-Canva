d87f3d1f831f81aa3604779bcaa08239de89da13
Madhwansh <madhwanshsrivastava@gmail.com>
2025-08-12 20:48:25 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,5 +10,6 @@ export default defineConfig({
   preview: {
     host: "0.0.0.0",
     port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+    allowedHosts: ["stateless-canva.onrender.com"],
   },
 });
